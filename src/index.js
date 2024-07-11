45af23d1ac5036a8643b0bd15a17133f0b9f8b75
farjadakbar4 <farjadakbar4@gmail.com>
2024-07-11 19:02:13 +0500

    feat: add select async

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { BoxProps, Box, H5, H2, Label, Illustration, Icon, Input, FormGroup, Button, Text, Image, MessageBox, MadeWithLove, Navigation, themeGet } from '@adminjs/design-system';
+import { BoxProps, Box, H5, H2, Label, Illustration, SelectAsync, Icon, Input, FormGroup, Button, Text, Image, MessageBox, MadeWithLove, Navigation, themeGet } from '@adminjs/design-system';
 import { ReduxState, useTranslation, ApiClient, ComponentLoader, OverridableComponent, useNavigationResources } from 'adminjs';
 import { styled } from '@adminjs/design-system/styled-components';
 
 export { 
     // design system
-    BoxProps, Box, H5, H2, Label, Illustration, Input, Icon, FormGroup, Button, Text, MessageBox, Image, MadeWithLove, Navigation, themeGet, 
+    BoxProps, Box, H5, H2, Label, Illustration, Input, SelectAsync, Icon, FormGroup, Button, Text, MessageBox, Image, MadeWithLove, Navigation, themeGet, 
     // adminjs
     ReduxState, useTranslation, ApiClient, ComponentLoader, OverridableComponent, useNavigationResources,
     // styled component
